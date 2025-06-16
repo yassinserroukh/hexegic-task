d96b4e4f59e45bd1be4768799a757730eadbc306
Yassin Serroukh <yassinserroukh00@gmail.com>
2025-06-17 00:28:03 +0100

    Test configuration setup

---
+++ jest.config.js
@@ -0,0 +1,20 @@
+export default {
+  testEnvironment: 'jsdom',
+  setupFilesAfterEnv: ['<rootDir>/src/setupTests.js'],
+  transform: {
+    '^.+\\.(js|jsx)$': [
+      'babel-jest',
+      {
+        presets: [
+          ['@babel/preset-env', { targets: { node: 'current' } }],
+          ['@babel/preset-react', { runtime: 'automatic' }],
+        ],
+      },
+    ],
+  },
+  moduleFileExtensions: ['js', 'jsx'],
+  extensionsToTreatAsEsm: ['.jsx'],
+  moduleNameMapper: {
+    '\\.(css|less|scss|sass)$': 'identity-obj-proxy',
+  },
+};
